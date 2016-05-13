9b9249eb24937a2e264542925c0c6074137000cf
semyenov <semyenov@hotmail.com>
2016-05-13 15:13:23 +0300

    lelokl site front-end init

---
+++ src/assets/js/common.js
@@ -0,0 +1,40 @@
+$(function() {
+
+  setInterval( function() {
+    var seconds = new Date().getSeconds();
+    var sdegree = seconds * 6;
+    var srotate = "rotate(" + sdegree + "deg)";
+
+    $("#sec").css({ "transform": srotate });
+
+  }, 1000 );
+
+  setInterval( function() {
+    var hours = new Date().getHours();
+    var mins = new Date().getMinutes();
+    var hdegree = hours * 30 + (mins / 2);
+    var hrotate = "rotate(" + hdegree + "deg)";
+
+    $("#hour").css({ "transform": hrotate});
+
+  }, 1000 );
+
+  setInterval( function() {
+    var mins = new Date().getMinutes();
+    var mdegree = mins * 6;
+    var mrotate = "rotate(" + mdegree + "deg)";
+
+    $("#min").css({ "transform" : mrotate });
+
+  }, 1000 );
+
+});
+
+
+$(document).ready(function() {
+//  $('.news .card-block').equalize();
+  $(".card-title, .card-text").dotdotdot({
+    watch: 'window',
+    height: '40px'
+  });
+});
